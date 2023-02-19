18d61694360c44c7e4497732cdb48f0998e8bab5
Shobhandeb Paul <55660915+herbert0419@users.noreply.github.com>
2023-02-19 21:34:51 +0530

    Update app_main.py

--- app_main.py
+++ app_main.py
@@ -1,33 +1,68 @@
-import yfinance as yf
 import numpy as np
-from sklearn.linear_model import LinearRegression
+import pandas as pd
+import yfinance as yf
+import streamlit as st
+from tensorflow.keras.models import load_model
+from sklearn.preprocessing import MinMaxScaler
 
-# Load historical data for ADANIENT.NS from Yahoo Finance
-ticker = "ADANIENT.NS"
-df = yf.download(ticker, start="2020-01-01", end="2022-02-18")
+# Load the pre-trained LSTM model and scaler
+model = load_model('keras_model.h5')
+scaler = MinMaxScaler(feature_range=(0, 1))
 
-# Create features and target variables
-df["SMA_20"] = df["Close"].rolling(window=20).mean()
-df["SMA_50"] = df["Close"].rolling(window=50).mean()
-df["SMA_200"] = df["Close"].rolling(window=200).mean()
-df["Return"] = df["Close"].pct_change()
-df.dropna(inplace=True)
+# Get the data using the yfinance API
+data = yf.download('ADANIENT.NS', period='max')
+data = data.sort_index(ascending=True, axis=0)
 
-X = df[["SMA_20", "SMA_50", "SMA_200", "Return"]].values
-y = df["Close"].values
+# Create a new dataframe with only the 'Close' column
+new_data = pd.DataFrame(index=range(0,len(data)),columns=['Close'])
+for i in range(0,len(data)):
+    new_data['Close'][i] = data['Close'][i]
 
-# Split data into training and testing sets
-n = len(df)
-train_X, train_y = X[:int(n * 0.8)], y[:int(n * 0.8)]
-test_X, test_y = X[int(n * 0.8):], y[int(n * 0.8):]
+# Normalize the data using the scaler
+scaled_data = scaler.fit_transform(np.array(new_data).reshape(-1,1))
 
-# Train linear regression model
-model = LinearRegression()
-model.fit(train_X, train_y)
+# Define the function to create the dataset
+def create_dataset(dataset, time_step=1):
+    X_data, y_data = [], []
+    for i in range(len(dataset)-time_step-1):
+        a = dataset[i:(i+time_step), 0]
+        X_data.append(a)
+        y_data.append(dataset[i + time_step, 0])
+    return np.array(X_data), np.array(y_data)
 
-# Make predictions on test set
-predictions = model.predict(test_X)
+# Set the time step for the dataset
+time_step = 100
 
-# Calculate mean absolute error (MAE) on test set
-mae = np.mean(np.abs(predictions - test_y))
-print("Mean absolute error:", mae)
+# Create the X dataset
+x_input = scaled_data[len(scaled_data)-time_step:].reshape(1,-1)
+temp_input = list(x_input)
+temp_input = temp_input[0].tolist()
+
+# Define the number of days to predict
+n_days = 7
+
+# Make the predictions
+lst_output=[]
+n_steps=time_step
+i=0
+while(i<n_days):
+    if(len(temp_input)>time_step):
+        x_input=np.array(temp_input[1:])
+        x_input=x_input.reshape(1,-1)
+        x_input = x_input.reshape((1, n_steps, 1))
+        yhat = model.predict(x_input, verbose=0)
+        temp_input.append(yhat[0][0])
+        temp_input=temp_input[1:]
+        lst_output.append(yhat[0][0])
+        i+=1
+
+# Inverse transform the predictions to get the actual values
+predictions = scaler.inverse_transform(np.array(lst_output).reshape(-1, 1))
+
+# Set up the Streamlit app
+st.title('ADANIENT.NS Stock Price Prediction using LSTM and yfinance API')
+st.subheader('Predicted Stock Price for the next {} days'.format(n_days))
+st.line_chart(predictions)
+
+st.subheader('Actual Stock Price for the last {} days'.format(n_days))
+st.line_chart(data['Close'][-n_days:])
